3fc68368c1b5fa79b9c12da28645b5dfa7db57f4
PitsMB <balagtasmichaeljoshua@gmail.com>
2024-06-18 12:49:22 +0800

    GET, POST and PUT Method items(hardcoded)

--- src/app.py
+++ src/app.py
@@ -1,30 +1,43 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+items = [
+    {
+        "id": 1,
+        "name": "Shoes1"
+    },
+    {
+        "id": 2,
+        "name": "Shoes2"
+    },
+    {
+        "id": 3,
+        "name": "Shoes3"
+    }
+]
+
 @app.route('/api/items', methods=['GET'])
 def get_items():
-    items = [
-        {
-            "id": 1,
-            "name": "Shoes1"
-        },
-        {
-            "id": 2,
-            "name": "Shoes2"
-        },
-        {
-            "id": 3,
-            "name": "Shoes3"
-        }
-    ]
+    return jsonify(items)
+
+@app.route('/api/items', methods=['POST'])
+def add_items():
+    item = request.get_json()
+    items.append(item)
+    return jsonify(items)
+
+@app.route('/api/items/<item_id>', methods=['PUT'])
+def edit_items(item_id):
+    new_name = request.json['name']
+    new_item = {
+        "id": item_id,
+        "name": new_name
+    }
+
+    
     return jsonify(items)
 
 if __name__ == '__main__':
     app.run(debug=True)
     
-
-# @app.route('/api/items-details/', methods=['GET'])
-# def find_items():
-#     details = {'id': 1, 'name': 'shoes1'}
-#     return details
