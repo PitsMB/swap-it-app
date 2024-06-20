69f15d21d212c786baf31518bc5bf4ca8e227cfa
PitsMB <balagtasmichaeljoshua@gmail.com>
2024-06-20 23:36:10 +0800

    GEt, POST, PUT and DELETE methods for items (hardcoded)

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, redirect, Response
 
 app = Flask(__name__)
 
@@ -36,7 +36,32 @@ def get_item_by_name(item_name):
         if name_to_find['name'] == item_name:
             return_name_value = name_to_find
     return jsonify(return_name_value)
-    
+
+@app.route('/api/items', methods=['POST'])
+def add_items():
+    item = request.get_json()
+    items.append(item)
+    return jsonify(items)
+
+@app.route('/api/items/<int:id_to_edit>', methods=['PUT'])
+def edit_item_by_id(id_to_edit):
+    print(type(id_to_edit))
+    request_data = request.get_json()
+    updated_item = {
+        'id': id_to_edit,
+        'name': request_data['name'] 
+    }
+    for item in items:
+        if item['id'] == id_to_edit:
+            item.update(updated_item)
+    return jsonify(updated_item)
+
+@app.route('/api/items/<int:id_to_delete>', methods=['DELETE'])
+def delete_item(id_to_delete):
+    for index, item in enumerate(items):
+        if item['id'] == id_to_delete:
+            del items[index]
+    return jsonify(items)
 
 if __name__ == '__main__':
     app.run(debug=True)
